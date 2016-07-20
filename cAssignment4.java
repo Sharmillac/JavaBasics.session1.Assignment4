614e90f30b001fe0b423f4513e8ce480915b625a
Sharmillac <sharmillacordeiro9@hotmail.com>
2016-07-21 05:17:19 +0530

    Excercise 4

---
+++ cAssignment4.java
@@ -0,0 +1,22 @@
+public class cAssignment4 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        // x + y * 2. Answer = 25
+        // x - y + 2 Answer = -3
+        // (x + y) * 2 Answer = 30
+        // y % x Answer = 0
+        System.out.println(x + y * 2); // 25
+        System.out.println(x - y + 2); // -3
+        System.out.println((x + y) * 2); // 30
+        System.out.println(y % x); // 0
+        // BODMAS
+
+    }
+
+
+}
